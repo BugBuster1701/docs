6d3ea7af91d01268a752bafe537eb90097900a5e
BugBuster <contao.modul@glen-langer.de>
2016-06-02 23:19:12 +0200

    make pdf script for errorlog_enhanced

---
+++ manual/make-pdf-errorlog_enhanced.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /daten/books/bugbuster-docs/manual
+
+gitbook pdf errorlog_enhanced/ ./user-guide-errorlog_enhanced.pdf
+
+#cp bb_changelog_gen/cover_de.jpg bb_changelog_gen/cover.jpg
+#gitbook pdf   bb_changelog_gen/ ./user-guide-bb_changelog_gen-temp.pdf
+#mv user-guide-bb_changelog_gen-temp_de.pdf user-guide-bb_changelog_gen_de.pdf
+#cp bb_changelog_gen/cover_en.jpg bb_changelog_gen/cover.jpg
+#gitbook pdf   bb_changelog_gen/ ./user-guide-bb_changelog_gen-temp.pdf
+#mv user-guide-bb_changelog_gen-temp_en.pdf user-guide-bb_changelog_gen_en.pdf
+#rm user-guide-bb_changelog_gen-temp_de.pdf
+#rm bb_changelog_gen/cover.jpg
