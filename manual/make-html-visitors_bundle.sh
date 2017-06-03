c4b0109168376df5ace510e806bd4de2277866ef
Glen Langer <contao.modul@glen-langer.de>
2017-06-03 22:46:00 +0200

    new cover

--- manual/make-html-visitors_bundle.sh
+++ manual/make-html-visitors_bundle.sh
@@ -2,6 +2,7 @@
 
 cd /daten/books/bugbuster-docs/manual
 
+# gitbook install visitors_bundle/
 gitbook build visitors_bundle/
 
 html5validator --root visitors_bundle/_book/ --ignore "gitbook/plugins"
